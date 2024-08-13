071f564a37d53354a0f50fa8957b22fd29b65648
BazilSuhail <bazil1854@gmail.com>
2024-08-13 19:00:19 +0500

    updated app.js ...

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { ActivityIndicator, View } from 'react-native'; // Import Text component
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Provider } from 'react-redux'; // Import Provider from react-redux
+import store from './redux/store'; // Import your Redux store
+import AppNavigator from './AppNavigator';
+import Signin from './Components/Authentication/Signin'; // Import your SignIn screen
 
-export default function App() {
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      try {
+        const token = await AsyncStorage.getItem('token');
+        if (token) {
+          console.log('Token found:', token); // Debug log
+        } else {
+          console.log('No token found'); // Debug log
+          setIsAuthenticated(false);
+
+        }
+        console.log(isAuthenticated); // Debug log
+        setIsAuthenticated(!!token); // Check if token exists
+      } catch (error) {
+        console.error('Error checking authentication:', error.message); // Enhanced error logging
+        setIsAuthenticated(false);
+      }
+    };
+
+    checkAuthentication();
+  }, []);
+ 
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}> {/* Wrap the application with Provider */}
+      <NavigationContainer>
+        {isAuthenticated ? <AppNavigator /> : <Signin />}
+      </NavigationContainer>
+    </Provider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
