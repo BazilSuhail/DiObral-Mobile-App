959d27b693d3b12959ed7ee29ae8b9e2350bda1d
BazilSuhail <bazil1854@gmail.com>
2024-12-31 00:49:24 +0500

    Removed unwanted "expo-splash-screen" module

--- app/_layout.js
+++ app/_layout.js
@@ -1,26 +1,16 @@
 import { Stack } from 'expo-router';
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import { useFonts } from 'expo-font'; 
+import { StatusBar } from 'expo-status-bar'; 
 import 'react-native-reanimated';
 import { Provider } from 'react-redux';
 import store from '@/hooks/store';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
 
   if (!loaded) {
     return null;
